519095aac8064540ae806f6d2b3ce7e8bbc3714a
Caleb Taylor <aquaductape@gmail.com>
2022-04-13 10:55:41 -0700

    Added question dialog

--- src/pages/Submissions.tsx
+++ src/pages/Submissions.tsx
@@ -14,7 +14,13 @@ import {
 import Loader from "../components/Loader";
 import JSConfetti from "js-confetti";
 import { NavLink, useParams, useRouteData } from "solid-app-router";
-import { link, code, star } from "solid-heroicons/outline";
+import {
+  link,
+  code,
+  star,
+  x,
+  questionMarkCircle,
+} from "solid-heroicons/outline";
 import { star as solidStar } from "solid-heroicons/solid";
 import { Icon } from "solid-heroicons";
 import { chevronLeft } from "solid-heroicons/outline";
@@ -258,6 +264,106 @@ const DropdownButton: Component<{
   );
 };
 
+const QuestionModal: Component = () => {
+  const lsKey = "submission-dialog-rules";
+  const getLS = () => {
+    try {
+      const resultStr = localStorage.getItem(lsKey)!;
+      if (!resultStr) return true;
+      const result = JSON.parse(resultStr);
+      return result.open === "true";
+    } catch {
+      return true;
+    }
+  };
+  const [toggle, setToggle] = createSignal(getLS());
+  let btnEl!: HTMLButtonElement;
+
+  onMount(() => {
+    localStorage.setItem(lsKey, JSON.stringify({ open: true }));
+  });
+
+  return (
+    <>
+      <button
+        class="fixed bottom-0 left-0 text-solid-medium p-2 w-11 h-11 z-50"
+        aria-label="open questions about submissions dialog"
+        ref={btnEl}
+      >
+        <Icon stroke-width={1.5} path={questionMarkCircle} />
+      </button>
+      <Dismiss menuButton={btnEl} open={toggle} setOpen={setToggle} modal>
+        <div class="fixed top-0 left-0 bg-slate-900/20 h-screen w-screen flex justify-center items-center">
+          <div
+            class="relative bg-white rounded-lg shadow-lg p-6 py-12 mx-3"
+            role="dialog"
+          >
+            <p class="text-lg">
+              You have <strong>three votes</strong>{" "}
+              <span class="inline-flex space-x-1 text-solid-medium relative top-[3px]">
+                <For each={[0, 0, 0]}>
+                  {() => <Icon class="w-4 h-4" path={star} />}
+                </For>
+              </span>{" "}
+              per category.
+            </p>
+            <br />
+            <p>There are three categories in total:</p>
+            <ul class="ml-3 my-3 flex flex-col space-y-3">
+              <li class="flex space-x-3 items-center">
+                <span>Best App</span>
+                <img
+                  class="inline-block rounded-full w-8 h-8 bg-solid-medium p-1"
+                  src="/img/award-best-app.svg"
+                  alt="award"
+                />
+              </li>
+              <li class="flex space-x-3 items-center">
+                <span>Best Ecosystem Utility</span>
+                <img
+                  class="inline-block rounded-full w-8 h-8 bg-solid-medium p-1"
+                  src="/img/award-ecosystem.svg"
+                  alt="award"
+                />
+              </li>
+              <li class="flex space-x-3 items-center">
+                <span>Best Student Project</span>
+                <img
+                  class="inline-block rounded-full w-8 h-8 bg-solid-medium p-1"
+                  src="/img/award-student-project.svg"
+                  alt="award"
+                />
+              </li>
+            </ul>
+            <br />
+            <p>
+              So in total you have 9 votes, but you can only caste 3 votes per
+              category. <br /> Casted vote is a filled in yellow star{" "}
+              <span class="inline-block relative top-[3px]">
+                <Icon class="w-4 h-4 text-star" path={solidStar} />
+              </span>
+            </p>
+            <div class="mt-8 -mb-6">
+              <button
+                class="bg-solid-default rounded-md text-white px-5 py-2"
+                onClick={() => setToggle(false)}
+              >
+                Let's Vote!
+              </button>
+            </div>
+            <button
+              class="absolute top-0 right-0 p-2"
+              onClick={() => setToggle(false)}
+            >
+              <Icon class="w-8 h-8" path={x} />
+            </button>
+          </div>
+        </div>
+      </Dismiss>
+    </>
+  );
+};
+
 const Submissions: Component = () => {
   const context = useAppContext();
   const params = useParams<{ category: "best_app" }>();
@@ -325,28 +431,30 @@ const Submissions: Component = () => {
   onMount(() => (jsConfetti = new JSConfetti({ canvas: canvasRef })));
 
   return (
-    <SubmissionContext.Provider
-      value={{
-        votes,
-        playConfetti,
-        updateVotes: mutate,
-      }}
-    >
-      <canvas
-        ref={(ref) => (canvasRef = ref)}
-        class="h-full w-full fixed top-0 left-0"
-      />
-      <div class="w-full lg:py-6 bg-hack bg-no-repeat bg-top bg-contain bg-fixed">
-        <div class="container pt-[18px] px-7 grid lg:pt-0 lg:grid-cols-10 relative">
-          <div class="fixed top-0 left-0 w-full h-[220px] z-1 lg:hidden bg-cover bg-image-[url(/img/hack/banner.png),linear-gradient(180deg,white_45%,transparent_60%)] md:bg-image-[url(/img/hack/banner.png),linear-gradient(180deg,white_70%,transparent_85%)] pointer-events-none"></div>
-          <div
-            class={`
-          sticky top-16 lg:top-20 lg:col-span-2 leading-7 h-max z-1 rounded-lg pointer-events-none
-          `}
-            ref={stickyContainerEl}
-          >
+    <>
+      <QuestionModal />
+      <SubmissionContext.Provider
+        value={{
+          votes,
+          playConfetti,
+          updateVotes: mutate,
+        }}
+      >
+        <canvas
+          ref={(ref) => (canvasRef = ref)}
+          class="h-full w-full fixed top-0 left-0"
+        />
+        <div class="w-full lg:py-6 bg-hack bg-no-repeat bg-top bg-contain bg-fixed">
+          <div class="container pt-[18px] px-7 grid lg:pt-0 lg:grid-cols-10 relative">
+            <div class="fixed top-0 left-0 w-full h-[220px] z-1 lg:hidden bg-cover bg-image-[url(/img/hack/banner.png),linear-gradient(180deg,white_45%,transparent_60%)] md:bg-image-[url(/img/hack/banner.png),linear-gradient(180deg,white_70%,transparent_85%)] pointer-events-none"></div>
             <div
               class={`
+          sticky top-16 lg:top-20 lg:col-span-2 leading-7 h-max z-1 rounded-lg pointer-events-none
+          `}
+              ref={stickyContainerEl}
+            >
+              <div
+                class={`
             pb-[35px]
             bg-gradient-transparent-40%-white-40% has-backdrop-filter:bg-gradient-white-40_percent-to-white-50_percent 
             mask-image-[linear-gradient(to_bottom,black_calc(100%-30px),transparent)]
@@ -354,114 +462,115 @@ const Submissions: Component = () => {
             lg:mask-image-none
             lg:bg-none
           `}
-            >
-              <div class="grid gap-2 grid-cols-[1fr,110px] md:grid-cols-[1fr,160px] lg:grid-cols-full lg:px-4 pointer-events-auto">
-                <div class="md:hidden relative w-full">
-                  <DropdownButton
-                    title={categories[params.category].title}
-                    image={categories[params.category].image}
-                    active={toggle()}
-                    ref={buttonEl}
-                  />
-                  <Dismiss
-                    menuButton={buttonEl}
-                    open={toggle}
-                    setOpen={setToggle}
-                    mount={stickyContainerEl}
-                    animation={{
-                      enterClass: "opacity-0 translate-x-[-20%]",
-                      enterToClass:
-                        "opacity-1 translate-x-0 transition-composite duration-200",
-                      exitClass: "opacity-1 translate-x-0",
-                      exitToClass:
-                        "opacity-0 translate-x-[-20%] transition-composite duration",
-                      appendToElement: "menuPopup",
-                    }}
-                  >
-                    <div class="absolute top-16 left-0 w-full bg-white rounded-lg pl-5 shadow-lg pointer-events-auto">
-                      <For each={Object.entries(categories)}>
-                        {([id, { title, image }]) => {
-                          return (
-                            <CategoryItem
-                              id={id}
-                              title={title}
-                              image={image}
-                              onClick={() => {
-                                setToggle(false);
-                              }}
-                            />
-                          );
-                        }}
-                      </For>
-                    </div>
-                  </Dismiss>
-                </div>
-                <div class="hidden md:grid gap-2 md:grid-cols-[1fr,1fr,1fr] lg:grid-cols-full">
-                  <For each={Object.entries(categories)}>
-                    {([id, { image, title }]) => {
-                      return (
-                        <CategoryButton id={id} title={title} image={image} />
-                      );
-                    }}
-                  </For>
-                </div>
-                <Show when={context.user}>
-                  <Show
-                    fallback={
-                      <div class="flex py-8 justify-center">
-                        Loading votes...
+              >
+                <div class="grid gap-2 grid-cols-[1fr,110px] md:grid-cols-[1fr,160px] lg:grid-cols-full lg:px-4 pointer-events-auto">
+                  <div class="md:hidden relative w-full">
+                    <DropdownButton
+                      title={categories[params.category].title}
+                      image={categories[params.category].image}
+                      active={toggle()}
+                      ref={buttonEl}
+                    />
+                    <Dismiss
+                      menuButton={buttonEl}
+                      open={toggle}
+                      setOpen={setToggle}
+                      mount={stickyContainerEl}
+                      animation={{
+                        enterClass: "opacity-0 translate-x-[-20%]",
+                        enterToClass:
+                          "opacity-1 translate-x-0 transition-composite duration-200",
+                        exitClass: "opacity-1 translate-x-0",
+                        exitToClass:
+                          "opacity-0 translate-x-[-20%] transition-composite duration",
+                        appendToElement: "menuPopup",
+                      }}
+                    >
+                      <div class="absolute top-16 left-0 w-full bg-white rounded-lg pl-5 shadow-lg pointer-events-auto">
+                        <For each={Object.entries(categories)}>
+                          {([id, { title, image }]) => {
+                            return (
+                              <CategoryItem
+                                id={id}
+                                title={title}
+                                image={image}
+                                onClick={() => {
+                                  setToggle(false);
+                                }}
+                              />
+                            );
+                          }}
+                        </For>
                       </div>
-                    }
-                    when={!votes.loading}
-                  >
-                    <div class="md:pt-5 text-center text-[11px] lg:text-xs rounded-lg md:bg-gradient-white/0.95-15%-to-transparent lg:bg-none">
-                      <span class="hidden md:block">
-                        Remaining category votes
-                      </span>
-                      <span class="md:hidden">Remaining votes</span>
-                      <div class="md:mt-3 flex px-3 justify-between">
-                        <For each={votes()[params.category].selections}>
-                          {() => (
+                    </Dismiss>
+                  </div>
+                  <div class="hidden md:grid gap-2 md:grid-cols-[1fr,1fr,1fr] lg:grid-cols-full">
+                    <For each={Object.entries(categories)}>
+                      {([id, { image, title }]) => {
+                        return (
+                          <CategoryButton id={id} title={title} image={image} />
+                        );
+                      }}
+                    </For>
+                  </div>
+                  <Show when={context.user}>
+                    <Show
+                      fallback={
+                        <div class="flex py-8 justify-center">
+                          Loading votes...
+                        </div>
+                      }
+                      when={!votes.loading}
+                    >
+                      <div class="md:pt-5 text-center text-[11px] lg:text-xs rounded-lg md:bg-gradient-white/0.95-15%-to-transparent lg:bg-none">
+                        <span class="hidden md:block">
+                          Remaining category votes
+                        </span>
+                        <span class="md:hidden">Remaining votes</span>
+                        <div class="md:mt-3 flex px-3 justify-between">
+                          <For each={votes()[params.category].selections}>
+                            {() => (
+                              <button class="pointer">
+                                <Icon
+                                  class="w-full h-full lg:w-12 text-star"
+                                  path={solidStar}
+                                />
+                              </button>
+                            )}
+                          </For>
+                          <Repeat
+                            times={
+                              votes()[params.category].total -
+                              votes()[params.category].selections.length
+                            }
+                          >
                             <button class="pointer">
                               <Icon
-                                class="w-full h-full lg:w-12 text-star"
-                                path={solidStar}
+                                class="w-full h-full lg:w-12 text-gray-400"
+                                path={star}
                               />
                             </button>
-                          )}
-                        </For>
-                        <Repeat
-                          times={
-                            votes()[params.category].total -
-                            votes()[params.category].selections.length
-                          }
-                        >
-                          <button class="pointer">
-                            <Icon
-                              class="w-full h-full lg:w-12 text-gray-400"
-                              path={star}
-                            />
-                          </button>
-                        </Repeat>
+                          </Repeat>
+                        </div>
                       </div>
-                    </div>
+                    </Show>
                   </Show>
-                </Show>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="lg:col-span-8 rounded-lg leading-7 shadow-2xl bg-white/80 min-h-[1500px]">
-            <ul class="divide-y divide-gray-200">
-              <For each={data.submissions}>
-                {(submission, index) => (
-                  <SubmissionRow index={index()} {...submission} />
-                )}
-              </For>
-            </ul>
+            <div class="lg:col-span-8 rounded-lg leading-7 shadow-2xl bg-white/80 min-h-[1500px]">
+              <ul class="divide-y divide-gray-200">
+                <For each={data.submissions}>
+                  {(submission, index) => (
+                    <SubmissionRow index={index()} {...submission} />
+                  )}
+                </For>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </SubmissionContext.Provider>
+      </SubmissionContext.Provider>
+    </>
   );
 };
 
