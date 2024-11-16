3e26736f3d0eced10ad94a0bc8c9870d97273f70
David Di Biase <1168397+davedbase@users.noreply.github.com>
2024-11-16 14:17:31 -0500

    Changed publish date

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -141,7 +141,7 @@ export default function Home() {
           </small>
         </div>
         <div class="text-center text-sm p-10">
-          Last updated November 14, 2024 @ 10:00pm EST
+          Last updated November 16, 2024 @ 2:00pm EST
         </div>
       </div>
     </>
